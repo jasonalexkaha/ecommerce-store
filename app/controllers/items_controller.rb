2cc175a4ea9bbc0c847ab1d5ec54d06eea77bf9f
Jason Alexander Kaharudin <jason@Jasons-MacBook-Air.local>
2022-09-05 17:57:17 +0800

    brands CRUD & items CRUD done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,36 @@
 class ItemsController < ApplicationController
+    def new
+        @brand = Brand.find(params[:brand_id])
+        @item = @brand.items.new
+    end
+
+    def edit
+        @brand = Brand.find(params[:brand_id])
+        @item = @brand.items.find(params[:id])
+    end
+
     def create
         @brand = Brand.find(params[:brand_id])
         @item = @brand.items.create(item_params)
+
+        redirect_to brand_path(@brand)
+    end
+
+    def update
+        @brand = Brand.find(params[:brand_id])
+        @item = @brand.items.find(params[:id])
+
+        if @item.update(item_params)
+            redirect_to brand_path(@brand)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @brand = Brand.find(params[:brand_id])
+        @item = @brand.items.find(params[:id])
+        @item.destroy
 
         redirect_to brand_path(@brand)
     end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,6 @@
 class Brand < ApplicationRecord
     has_one_attached :image
-    has_many :items
+    has_many :items, dependent: :delete_all
 
     validates :name, presence: true
 end
